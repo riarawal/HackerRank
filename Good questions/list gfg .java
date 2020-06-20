f0b85f5a6b070ca90a1c63078c58844774713da5
riarawal <54265567+riarawal@users.noreply.github.com>
2020-06-20 19:51:42 +0530

    Create list gfg .java

---
+++ Good questions/list gfg .java
@@ -0,0 +1,1 @@
+https://www.geeksforgeeks.org/count-occurrences-elements-list-java/#:~:text=To%20count%20occurrences%20of%20elements,o%20in%20the%20collection%20c.&text=%2F%2F%20using%20HashSet%20and%20Collections.
